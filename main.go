e110bfe6e49d15c2479d907932ac1a862d22026a
Austin Cunningham <aucunnin@redhat.com>
2018-12-21 16:18:31 +0000

    PUT endpoint updates disabled and disabled message

--- main.go
+++ main.go
@@ -11,22 +11,47 @@ type App struct {
   ID        string   `json:"id,omitempty"`
   Appname   string   `json:"Appname,omitempty"`
   Disabled  bool     `json:"disabled,omitempty"`
+  GlobalDisableMessage string `json:"globalDisableMessage,omitempty"`
   Versions  *Versions `json:"versions,omitempty"`
 }
 
 //Versions struct
 type Versions struct {
   Version  string `json:"version,omitempty"`
-  Disabled bool   `json:"Disabled,omitempty"`
+  Disabled bool   `json:"disabled,omitempty"`
+  DisableMessage string `json:"disableMessage,omitempty"`
 }
 var apps []App
 
 func main() {
   fmt.Println("App served on http://localhost:8000")
   //populate the array
-  apps = append(apps, App{ID: "1", Appname: "MDC", Disabled: false, Versions: &Versions{Version: "1.1.1", Disabled: false}})
-  apps = append(apps, App{ID: "2", Appname: "Integreatly", Disabled: false, Versions: &Versions{Version: "1.0.1", Disabled: false}})
-  apps = append(apps, App{ID: "3", Appname: "RHMAP", Disabled: true, Versions: &Versions{Version: "4.6.2", Disabled: true}})
+  apps = append(apps, App{
+    ID: "1",
+    Appname: "MDC", 
+    Disabled: false, 
+    GlobalDisableMessage: "Disabled", 
+    Versions: &Versions{
+      Version: "1.1.1", 
+      Disabled: false}})
+  apps = append(apps, App{
+    ID: "2", 
+    Appname: "Integreatly", 
+    Disabled: false,
+    GlobalDisableMessage: "Disabled", 
+    Versions: &Versions{
+      Version: "1.0.1", 
+      Disabled: false,
+      DisableMessage: "Disabled by admin"}})
+  apps = append(apps, App{
+    ID: "3", 
+    Appname: "RHMAP", 
+    Disabled: true,
+    GlobalDisableMessage: "Disabled", 
+    Versions: &Versions{
+      Version: "4.6.2", 
+      Disabled: true,
+      DisableMessage: "Disabled by admin"}})
 
   // File server for public directroy
   http.Handle("/", http.FileServer(http.Dir("./public")))
@@ -48,8 +73,8 @@ func GetAllApps(w http.ResponseWriter, r *http.Request) {
 
 //GetApp get
 func GetApp(w http.ResponseWriter, r *http.Request) {
-  fmt.Println("http://localhost:8000/apps/{id} GET")
   params := mux.Vars(r)
+  fmt.Println("http://localhost:8000/apps/{id} GET id :", params["id"])
   for _, item := range apps {
       if item.ID == params["id"] {
           json.NewEncoder(w).Encode(item)
@@ -61,5 +86,19 @@ func GetApp(w http.ResponseWriter, r *http.Request) {
 
 //UpdateApp put
 func UpdateApp(w http.ResponseWriter, r *http.Request) {
-  fmt.Println("http://localhost:8000/apps/{id} PUT")
+  params := mux.Vars(r)
+  fmt.Println("http://localhost:8000/apps/{id} PUT id :", params["id"])
+  for _, item := range apps {
+    if item.ID == params["id"] {
+      // logic go in here for handeling the update e.g. {2 Integreatly false 0xc00000c320}
+       fmt.Println(item.Versions.Disabled)
+       fmt.Println(item.Versions.DisableMessage)
+       var disabled = &item.Versions.Disabled 
+       *disabled = true 
+       var disabledmessage = &item.Versions.DisableMessage
+       *disabledmessage = "Disabled by your admin"
+       fmt.Println(item.Versions.Disabled)
+       fmt.Println(item.Versions.DisableMessage)
+    }
+  }
 }
