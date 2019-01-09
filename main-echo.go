a014ae62a6ee281310d40fbcb8ee4687c8e52cca
Austin Cunningham <aucunnin@redhat.com>
2019-01-09 14:51:25 +0000

    GetApp working from postgres now

--- main-echo.go
+++ main-echo.go
@@ -1,49 +1,53 @@
 package main
 
-import(	
-	"net/http"
+import (
 	"database/sql"
-	"github.com/labstack/echo"
 	"fmt"
-	_"github.com/lib/pq"
+	"net/http"
+
+	"github.com/labstack/echo"
+	_ "github.com/lib/pq"
 )
-// posgres vars
+
+// posgres vars should be using env vars for this
 var db *sql.DB
+
 const (
-  dbhost = "localhost"
-  dbport = "5432"
-  dbuser = "postgres"
-  dbpass = "password"
-  dbname = "postgres"
+	dbhost = "localhost"
+	dbport = "5432"
+	dbuser = "postgres"
+	dbpass = "password"
+	dbname = "postgres"
 )
 
 //App struct
 type App struct {
-	ID        string   `json:"id,omitempty"`
-	Appname   string   `json:"Appname,omitempty"`
-	Disabled  bool     `json:"disabled,omitempty"`
-	GlobalDisableMessage string `json:"globalDisableMessage,omitempty"`
-	Versions  *Versions `json:"versions,omitempty"`
-  }
-  
+	ID                   string    `json:"id,omitempty"`
+	Appname              string    `json:"Appname,omitempty"`
+	Disabled             bool      `json:"disabled,omitempty"`
+	GlobalDisableMessage string    `json:"globalDisableMessage,omitempty"`
+	Versions             *Versions `json:"versions,omitempty"`
+}
+
 //Versions struct
 type Versions struct {
-Version  string `json:"version,omitempty"`
-Disabled bool   `json:"disabled,omitempty"`
-DisableMessage string `json:"disableMessage,omitempty"`
+	Version        string `json:"version,omitempty"`
+	Disabled       bool   `json:"disabled,omitempty"`
+	DisableMessage string `json:"disableMessage,omitempty"`
 }
+
 var apps []App
 
-func main(){
+func main() {
 	dbConnect()
 	defer db.Close()
 	populateArray()
 
 	router := echo.New()
-	router.GET("/", func(c echo.Context) error{
+	router.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello world!")
 	})
-	 
+
 	router.GET("/apps", GetAllApps)
 	router.GET("/apps/:id", GetApp)
 	//router.PUT("/apps/:id",)
@@ -61,40 +65,54 @@ func GetAllApps(c echo.Context) error {
 		fmt.Println(err)
 	}
 	defer rows.Close()
-	
-	var result []App
 
-	for rows.Next(){
+	// creates a new object from return from postgres
+	var result []App
+	for rows.Next() {
 		var app App
-		err2 := rows.Scan(&app.ID,&app.Appname,&app.Disabled,&app.GlobalDisableMessage)
+		err2 := rows.Scan(&app.ID, &app.Appname, &app.Disabled, &app.GlobalDisableMessage)
 		if err2 != nil {
 			return err2
 		}
-	 	result = append(result, app)
+		result = append(result, app)
 	}
+	// returns new object
 	return c.JSON(http.StatusOK, result)
-
 }
 
 // GetApp gets an app by id
 func GetApp(c echo.Context) error {
 	id := c.Param("id")
-	fmt.Println("id passed in : ",id)
+	fmt.Println("id passed in : ", id)
+	var app App
 	// using hard coded data
-	for _, item := range apps {
-		if item.ID == id {
-			return c.JSON(http.StatusOK, item)
-		}
+	// for _, item := range apps {
+	// 	if item.ID == id {
+	// 		return c.JSON(http.StatusOK, item)
+	// 	}
+	// }
+	// return c.JSON(http.StatusBadRequest, id)
+	sqlStatment := `SELECT id, appname, disabled, globaldisablemessage FROM apps WHERE id=$1;`
+	row := db.QueryRow(sqlStatment, id)
+	err := row.Scan(&app.ID, &app.Appname, &app.Disabled, &app.GlobalDisableMessage)
+	if err != nil {
+		return err
 	}
-	return c.JSON(http.StatusBadRequest, id)
+    // string casting
+    // app.ID = strconv.Itoa(id)
+    // app.Appname = appname
+    // app.Disabled = disabled
+    // app.GlobalDisableMessage = globaldisablemessage
+    // fmt.Println(id, appname, disabled, globaldisablemessage)
+	return c.JSON(http.StatusOK, app)
 }
 
 // no real difference here
-func dbConnect(){
+func dbConnect() {
 	var err error
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-	"password=%s dbname=%s sslmode=disable", dbhost, dbport, dbuser, dbpass, dbname)
-	
+		"password=%s dbname=%s sslmode=disable", dbhost, dbport, dbuser, dbpass, dbname)
+
 	db, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
 		//using panic may not be best practice
@@ -107,33 +125,33 @@ func dbConnect(){
 	fmt.Println("Successfully connected to Posgres DB!")
 }
 
-func populateArray(){
+func populateArray() {
 	//var err error
-	//populate the array hard coded mock data at the moment 
+	//populate the array hard coded mock data at the moment
 	apps = append(apps, App{
-	  ID: "1",
-	  Appname: "MDC", 
-	  Disabled: false, 
-	  GlobalDisableMessage: "Disabled", 
-	  Versions: &Versions{
-		Version: "1.1.1", 
-		Disabled: false}})
+		ID:                   "1",
+		Appname:              "MDC",
+		Disabled:             false,
+		GlobalDisableMessage: "Disabled",
+		Versions: &Versions{
+			Version:  "1.1.1",
+			Disabled: false}})
 	apps = append(apps, App{
-	  ID: "2", 
-	  Appname: "Integreatly", 
-	  Disabled: false,
-	  GlobalDisableMessage: "Disabled", 
-	  Versions: &Versions{
-		Version: "1.0.1", 
-		Disabled: false,
-		DisableMessage: "Disabled by admin"}})
+		ID:                   "2",
+		Appname:              "Integreatly",
+		Disabled:             false,
+		GlobalDisableMessage: "Disabled",
+		Versions: &Versions{
+			Version:        "1.0.1",
+			Disabled:       false,
+			DisableMessage: "Disabled by admin"}})
 	apps = append(apps, App{
-	  ID: "3", 
-	  Appname: "RHMAP", 
-	  Disabled: true,
-	  GlobalDisableMessage: "Disabled", 
-	  Versions: &Versions{
-		Version: "4.6.2", 
-		Disabled: true,
-		DisableMessage: "Disabled by admin"}})
+		ID:                   "3",
+		Appname:              "RHMAP",
+		Disabled:             true,
+		GlobalDisableMessage: "Disabled",
+		Versions: &Versions{
+			Version:        "4.6.2",
+			Disabled:       true,
+			DisableMessage: "Disabled by admin"}})
 }
